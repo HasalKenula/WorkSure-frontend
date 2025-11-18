0ca8c1942acc5adfe6db0568910a8cc3407805a1
HasalKenula <hasalkenula255@gmail.com>
2025-11-18 21:29:26 +0530

    created protectedRoute, AuthContext for security

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,20 @@ import WorkerLogin from './pages/WorkerLogin'
 import WorkerProfileView from './pages/WorkerProfileView'
 import WorkerRegistration from './pages/WorkerRegistration'
 import WorkerSearch from './pages/WorkerSearch'
+import Category from './pages/Category'
+import { AuthProvider } from './context/AuthContext'
+import ProtectedRoute from './components/ProtectedRoute'
+import Login from './pages/auth/Login'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+     <AuthProvider>
     <BrowserRouter>
-      <Routes path="/">
+      <Routes>
+          <Route element={<ProtectedRoute />}>
         <Route path='/*' element={<HomePage />} />
         <Route path='/about' element={<AboutUs />} />
         <Route path='/contact' element={<ContactPage />} />
@@ -39,10 +45,14 @@ function App() {
         <Route path='/workerProfile' element={<WorkerProfileView />} />
         <Route path='/workerRegistration' element={<WorkerRegistration />} />
         <Route path='/workerDetails' element={<WorkerSearch />} />
-
+         <Route path='/category' element={<Category />} />
+         </Route>
+        <Route path="/auth/login" element={<Login />} />
+        
 
       </Routes>
     </BrowserRouter>
+    </AuthProvider>
   )
 }
 
